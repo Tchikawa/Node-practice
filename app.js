e7c6765b24ef98f4fbd495e2a784e8f6be46e4a9
Tchikawa <k10d2019@gmail.com>
2018-02-23 02:02:38 +0900

    Node.js超入門2-3

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const http = require('http');
 const fs = require('fs');
+const ejs = require('ejs');
+
+const index_page = fs.readFileSync('./index.ejs', 'utf-8');
 
 var server = http.createServer(getFromClient);
 server.listen(3000);
@@ -9,12 +12,17 @@ function getFromClient(req, res) {
   request = req;
   response = res;
 
-  fs.readFile('./index.html', 'UTF-8', (error, data) => {
-    var content = data
-      .replace(/dummy_title/g, 'タイトル')
-      .replace(/dummy_content/g, 'コンテンツ');
+  var content = ejs.render(index_page, {
+    title : "INDEX",
+    content : "これはテンプレートを使ったサンプルページです。"
+  });
+
+//  fs.readFile('./index.html', 'UTF-8', (error, data) => {
+//    var content = data
+//      .replace(/dummy_title/g, 'タイトル')
+//      .replace(/dummy_content/g, 'コンテンツ');
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(content);
     response.end();
-  });
+//  });
 }
