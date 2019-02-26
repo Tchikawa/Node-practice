cbee1e1381114999142d629c11373fdb201f29ff
Tchikawa <k10d2019@gmail.com>
2019-02-26 18:09:05 +0900

    バリデーション追加

--- ex-gen-app/app.js
+++ ex-gen-app/app.js
@@ -9,6 +9,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hello = require('./routes/hello');
 
+var validator = require('express-validator');
+
 var app = express();
 
 // view engine setup
@@ -20,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(validator());
 
 var session_opt = {
   secret: 'keyboard cat',
--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -28,18 +28,40 @@
    router.get('/add', (req, res, next) => {
      var data = {
        title: 'Hello/add',
-       content: '新しいレコードを入力'
+       content: '新しいレコードを入力',
+       form: {name:'', mail:'', age:0}
      }
      res.render('hello/add', data);
    });
 
    router.post('/add', (req, res, next) =>{
-     var nm = req.body.name;
-     var ml = req.body.mail;
-     var ag = req.body.age;
-     db.run('insert into mydata (name, mail, name) values (?, ?, ?)', nm, ml, ag);
-     res.redirect('/hello')
-   })
+    var response = res;
+    req.check('name', 'NAMEは必ず入力してください').notEmpty();
+    req.check('mail', 'MAILはメールアドレスを記入してください').isEmail();
+    req.check('age', 'AGEは年齢(整数)を入力してください').isInt();
+    req.getValidationResult().then((result) => {
+    if (!result.isEmpty()) {
+      var res = '<ul class="err">';
+      var result_arr = result.array();
+      for (var n in result_arr) {
+        res += '<li>' + result_arr[n].msg + '</li>';
+      }
+      res += '</ul>';
+      var data = {
+        title: 'Hello/show',
+        content: res,
+        form: req.body
+      }
+      response.render('hello/add', data);
+      } else {
+       var nm = req.body.name;
+       var ml = req.body.mail;
+       var ag = req.body.age;
+       db.run('insert into mydata (name, mail, name) values (?, ?, ?)', nm, ml, ag);
+       res.redirect('/hello')
+      }
+    })
+  });
 
    router.get('/show', (req, res, next) => {
      var id = req.query.id;
